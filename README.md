72d17ee996808d730197aa59b368e03e6ee716fb
Durban86 <durban86@verizon.net>
2023-01-31 11:16:39 -0500

    All files updated and app completed

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 https://www.theodinproject.com/lessons/foundations-etch-a-sketch
 
 1/30/23 - Creating a web based etch-a-sketch using HTML, CSS, and JS. 
+
+1/31/23 - App is working properly. When the "Make New Grid" button is clicked the user is prompted to enter a number of rows they wish the grid to have. When the cursor enters the grid it fills each cell it passes through. If there is no pen selected, the user will be alerted to select a pen when the cursor enters the grid. The black pen fills each cell with black with every pass through. The color pen randomly selects a new rgb code with every pass through a cell. The shader pen increases opacity by 10% with every pass through a cell.
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//Pseudocode
-//Create a grid based how many rows and columns the user chooses
-
 const container = document.getElementById('container');
 const btn = document.getElementById('newGrid');
+const blackPen = document.getElementById('blackPen');
+const colorPen = document.getElementById('colorPen');
+const shaderPen = document.getElementById('shaderPen');
 
-// let rows;
-// let cols;
+let pen;
 
 function removeAllChildNodes(parent) {
 	while (parent.firstChild) {
@@ -20,20 +19,57 @@ function createGrid(rows, cols) {
 	for (i = 0; i < (rows * cols); i++) {
 		let cell = document.createElement('div');
 		cell.addEventListener('mouseover', function(event) {
-			event.target.classList.add('color');
+			if (pen == 'blackPen') {
+				cell.style.background = '#000';
+			} else if (pen == 'colorPen') {
+				cell.style.background = randomColor();
+			} else if (pen == 'shaderPen') {
+				let currentOpacity = cell.style.opacity;
+				cell.style.background = 'rgb(0, 0, 0)'
+				if (currentOpacity) {
+					cell.style.opacity = Number(currentOpacity) + 0.1
+				} else {
+					cell.style.opacity = 0.1
+				}
+			} else {
+				alert('Please choose a pen')
+			}
 		});
 		container.appendChild(cell).className = 'grid-item';
 	}
 }
 
+function randomColor() {
+		let r = Math.floor(Math.random() * 256);
+		let g = Math.floor(Math.random() * 256);
+		let b = Math.floor(Math.random() * 256);
+		rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+		return rgb
+}
+
 btn.addEventListener('click', () => {
-	let gridItems = document.querySelectorAll('.grid-item')
 	let rows = prompt('How many rows should your grid have?');
 	let cols = rows;
 	createGrid(rows, cols);
 });
-// createGrid(rows, cols);
 
-// document.getElementsByClassName('grid-item').addEventListener('mouseover' () => {
-// 	document.getElementsByClassName.style.setProperty('background-color', 'black');
-// });
+blackPen.addEventListener('click', () => {
+	pen = blackPen.value
+	blackPen.classList.add('selected')
+	colorPen.classList.remove('selected')
+	shaderPen.classList.remove('selected')
+})
+
+colorPen.addEventListener('click', () => {
+	pen = colorPen.value
+	colorPen.classList.add('selected')
+	blackPen.classList.remove('selected')
+	shaderPen.classList.remove('selected')
+})
+
+shaderPen.addEventListener('click', () => {
+	pen = shaderPen.value
+	shaderPen.classList.add('selected')
+	colorPen.classList.remove('selected')
+	blackPen.classList.remove('selected')
+})
